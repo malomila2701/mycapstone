ff427d18debd4f274f8d73d3161a0f75a1ae2732
malomila2701 <jeansamu007@gmail.com>
2026-04-24 14:45:17 +0000

    tryFIX dbconnection

--- src/java/javafiles/DBConnection.java
+++ src/java/javafiles/DBConnection.java
@@ -9,22 +9,32 @@ package javafiles;
  * @author HP
  */
 public class DBConnection {
+
     public static java.sql.Connection connect() throws Exception {
-        
+
         // Essaie d'abord les variables Tomcat (JNDI) — local
+         String url, user, pass;
+         
         try {
             javax.naming.Context ctx = new javax.naming.InitialContext();
-            String url  = (String) ctx.lookup("java:comp/env/JDBC_URL");
-            String user = (String) ctx.lookup("java:comp/env/DB_USER");
-            String pass = (String) ctx.lookup("java:comp/env/DB_PASS");
+            url = (String) ctx.lookup("java:comp/env/JDBC_URL");
+            user = (String) ctx.lookup("java:comp/env/DB_USER");
+            pass = (String) ctx.lookup("java:comp/env/DB_PASS");
+            System.out.println(">>> JNDI OK");
             return java.sql.DriverManager.getConnection(url, user, pass);
-            
+
         } catch (javax.naming.NamingException e) {
             // Fallback sur variables d'environnement — Render
-            String url  = System.getenv("JDBC_URL");
-            String user = System.getenv("DB_USER");
-            String pass = System.getenv("DB_PASS");
+            url = System.getenv("JDBC_URL");
+            user = System.getenv("DB_USER");
+            pass = System.getenv("DB_PASS");
+
+            System.out.println(">>> JDBC_URL = " + url);
+            System.out.println(">>> DB_USER = " + user);
+            System.out.println(">>> DB_PASS is null? " + (pass == null));
+
             return java.sql.DriverManager.getConnection(url, user, pass);
+
         }
     }
 }
